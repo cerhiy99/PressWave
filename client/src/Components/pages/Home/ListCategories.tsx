c5c338958baaaa5e98d0f0353fb1a052b94da31f
cerhiy <cerhiy99@gmail.com>
2023-11-13 02:42:40 +0200

    fix listCategories

--- client/src/Components/pages/Home/ListCategories.tsx
+++ client/src/Components/pages/Home/ListCategories.tsx
@@ -7,8 +7,11 @@ import { useAction } from "../../../hoocks/useAcrion";
 
 const ListCategories = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const [hiddenCategories, setHiddenCategories] = useState<string[]>([]);
+  const [hiddenCategories, setHiddenCategories] = useState<
+    [{ id: number; name: string }] | []
+  >([]);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [isIcon, setIsIcon] = useState(true);
   const [windowWidth, setWindowWidth] = useState<number>(
     document.documentElement.clientWidth
   );
@@ -39,16 +42,40 @@ const ListCategories = () => {
     };
   }, []);
 
+  const setHiddenMenu = (index: number) => {
+    setHiddenCategories((prevState: any) => {
+      if (listCategories.length > 0) {
+        return listCategories.slice(index);
+      }
+      return prevState; // якщо listCategories порожній, повертаємо попередній стан
+    });
+    if (hiddenCategories.length === 0) {
+      setShowDropdown(false);
+    }
+  };
   useEffect(() => {
-    if (listCategories.length > 0) {
-      if (containerRef.current) {
-        containerRef.current.innerHTML = "";
+    if (listCategories.length > 0 && containerRef.current) {
+      containerRef.current.innerHTML = "";
+      setHiddenCategories([]);
 
-        for (let i = 0; i < listCategories.length; i++) {
-          containerRef.current.innerHTML += `
-              <div class="categories">
-                <p>${listCategories[i].name}</p>
-              </div>`;
+      const newDiv = document.createElement("div");
+      newDiv.className = "categories";
+
+      const newParagraph = document.createElement("p");
+      if (listCategories[0]) {
+        newParagraph.textContent = listCategories[0].name;
+        newDiv.appendChild(newParagraph);
+        containerRef.current.appendChild(newDiv);
+      }
+      let index = 0;
+      for (let i = 0; i < listCategories.length; i++) {
+        if (
+          containerRef.current.lastChild instanceof HTMLElement &&
+          containerRef.current.lastChild.className === "categories"
+        ) {
+          const newDiv1 = document.createElement("div");
+          newDiv1.className = "retreat";
+          containerRef.current.append(newDiv1);
           if (
             (windowWidth > 1024 &&
               containerRef.current.getBoundingClientRect().width >
@@ -58,15 +85,35 @@ const ListCategories = () => {
                 windowWidth - windowWidth / 20)
           ) {
             containerRef.current.lastChild?.remove();
-            if (listCategories.length >= i) {
-              setHiddenCategories(
-                listCategories.slice(i).map((item) => String(item.name))
-              );
-            }
-            break;
           }
         }
+        if (
+          containerRef.current.lastChild instanceof HTMLElement &&
+          containerRef.current.lastChild.className === "retreat"
+        ) {
+          const newDiv = document.createElement("div");
+          newDiv.className = "categories";
+
+          const newParagraph = document.createElement("p");
+          newParagraph.textContent = listCategories[i].name;
+          newDiv.appendChild(newParagraph);
+          containerRef.current.appendChild(newDiv);
+          index++;
+          if (
+            (windowWidth > 1024 &&
+              containerRef.current.getBoundingClientRect().width >
+                windowWidth - windowWidth / 10) ||
+            (windowWidth > 768 &&
+              containerRef.current.getBoundingClientRect().width >
+                windowWidth - windowWidth / 20)
+          ) {
+            containerRef.current.lastChild?.remove();
+            index--;
+            break;
+          }
+        } else continue;
       }
+      setHiddenMenu(index);
     }
   }, [windowWidth, listCategories]);
 
@@ -77,30 +124,33 @@ const ListCategories = () => {
     <div className="list-categories">
       <div className="list-categories-container">
         <div className="list-categories-value" ref={containerRef}></div>
-        {/*visibleCategories.map((x, idx) => (
-            <div key={idx} className="categories">
-              <p>{x}</p>
-            </div>
-          ))*/}
 
         {hiddenCategories.length > 0 && (
           <div className="container-hidden-categories" onClick={toggleDropdown}>
-            <div className="icon-dost">
-              <BsThreeDotsVertical />
-            </div>
-
-            {showDropdown && (
-              <div className="dropdown">
-                {hiddenCategories.map((x, idx) => (
-                  <div key={idx} className="hidden-categories">
-                    <p>{x}</p>
-                  </div>
-                ))}
+            {isIcon ? (
+              <div className="icon-dost">
+                <BsThreeDotsVertical />
               </div>
+            ) : (
+              <></>
             )}
           </div>
         )}
       </div>
+      {showDropdown ? (
+        <>
+          <div className="line" />
+          <div className="dropdown">
+            {hiddenCategories.map((x, idx) => (
+              <div key={x.id} className="hidden-categories">
+                <p>{x.name}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
